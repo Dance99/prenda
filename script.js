0991c1408d27e91c795833b3685ac4523d4d2359
Daniela <nam991019@gmail.com>
2024-10-31 17:29:17 -0500

    prenda

---
+++ script.js
@@ -0,0 +1,5 @@
+{//parseFloat es un método en Java que se utiliza para convertir una cadena de texto que representa un número decimal//
+//Usuario solicita precio de prenda//
+let precio = parseFloat(prompt("Ingresa el precio de la prenda de vestir"))
+//solicita el porcentaje del descuento//
+let descuento = parseFloat(prompt("ingresa el procentaje de descuento"))
