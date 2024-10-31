da4b52f50e91eab608f359e17c0cf4284a1a72d1
Daniela <nam991019@gmail.com>
2024-10-31 17:34:31 -0500

    Doocumetn prenda

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ let descuento = parseFloat(prompt("ingresa el procentaje de descuento"))
 //calcular descuento//
 let cantdescuento = (precio*descuento)/100;
 let preciofinal = precio-cantdescuento;
+
+//toFixed(2), el número se redondea al segundo decimal. Si el tercer decimal es 5 o más, el segundo decimal se redondea hacia arriba//
+//mostrar resultado//
+document.write("le precio original es: $" + precio.toFixed(2) + "<br>");
+document.write("El porcentaje de descuento es:" + descuento + "%<br>" )
+document.write("el precio final despues del descuento es: $" + preciofinal.toFixed(2))
+}
